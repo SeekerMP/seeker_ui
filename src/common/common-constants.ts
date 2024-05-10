516aae8070a120ed6f38809cde13eb92bcf41116
movorpovor <movorpovor@gmail.com>
2024-05-10 17:19:43 +0400

    add api configuration

--- src/common/common-constants.ts
+++ src/common/common-constants.ts
@@ -1,7 +1,8 @@
 import {createConfiguration, ServerConfiguration} from "../api-clients";
 
-const server = new ServerConfiguration<{  }>("http://localhost:5278", {  });
-
+// @ts-ignore
+const server = new ServerConfiguration<{  }>(window.BASE_URL, {  });
+console.log(window);
 export const configuration = createConfiguration({
     baseServer: server,
 });
